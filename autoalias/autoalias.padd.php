f53f3653a87daa2ece3c8d785717952c2b0e84a4
Alexey Konevets <Dr2005alex@mail.ru>
2011-02-06 07:38:55 +0000

    fix bugs (no title)

--- autoalias/autoalias.padd.php
+++ autoalias/autoalias.padd.php
@@ -10,8 +10,8 @@ Hooks=page.add.tags
 defined('COT_CODE') or die('Wrong URL');
 
 $t->assign(array(
-    "PAGEADD_FORM_TITLE" => "<input type=\"text\" class=\"text\" name=\"newpagetitle\" id=\"newpagetitle\" value=\"".$newpagetitle."\" size=\"56\" onchange=\"genSEF(this, document.forms['pageform'].newpagealias,1)\" onkeyup=\"genSEF(this,document.forms['pageform'].newpagealias,1)\" maxlength=\"255\" />",
-    "PAGEADD_FORM_ALIAS" => "<input type=\"text\" class=\"text\" name=\"newpagealias\" id=\"newpagealias\" value=\"".$newpagealias."\" size=\"16\" maxlength=\"255\" />"
+    "PAGEADD_FORM_TITLE" => "<input type=\"text\" class=\"text\" name=\"rpagetitle\" id=\"newpagetitle\" value=\"".$newpagetitle."\" size=\"56\" onchange=\"genSEF(this, document.forms['pageform'].newpagealias,1)\" onkeyup=\"genSEF(this,document.forms['pageform'].newpagealias,1)\" maxlength=\"255\" />",
+    "PAGEADD_FORM_ALIAS" => "<input type=\"text\" class=\"text\" name=\"rpagealias\" id=\"newpagealias\" value=\"".$newpagealias."\" size=\"16\" maxlength=\"255\" />"
 ));
 
 ?>
